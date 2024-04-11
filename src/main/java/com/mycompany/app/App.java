f7346d553ad9ece9223a78fa82a3363742b73a6d
Roberto <roberto.rodriguez.sanchez.1982@gmail.com>
2024-04-11 13:50:18 -0500

    Cambio en el mensaje 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Cibertec" );
+        System.out.println( "Hello World Cibertec parte 2" );
     }
 }
